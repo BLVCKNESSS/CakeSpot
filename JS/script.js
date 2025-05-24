96675628b734691f1ca35a8257f1e189135f9401
BLVCKNESSS <yan.tayouo22@gmail.com>
2025-05-24 01:52:23 +0100

    Premier commit

---
+++ JS/script.js
@@ -0,0 +1,345 @@
+const boissons = [
+  {
+    nom: "Genoise",
+    img: "../images/IMG_2830.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cake",
+    img: "../images/cake5.JPG",
+    prix: "700 F",
+    note: 5
+  },
+  {
+    nom: "marriage",
+    img: "../images/etage2.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cake",
+    img: "../images/IMG_2999.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Genoise",
+    img: "../images/IMG_2827.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Marriage",
+    img: "../images/etage1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake2.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Marriage",
+    img: "../images/etage3.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Genoise",
+    img: "../images/img_4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cake",
+    img: "../images/IMG_3001.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cupcake",
+    img: "../images/cupcake3.jpg",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Marriage",
+    img: "../images/etage4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "cake",
+    img: "../images/cake1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Genoise",
+    img: "../images/img_2.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake5.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cake",
+    img: "../images/cake3.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Genoise",
+    img: "../images/img_1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Marriage",
+    img: "../images/etage8.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake6.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Genoise",
+    img: "../images/img_3.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cake",
+    img: "../images/cake4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Marriage",
+    img: "../images/etage7.JPG",
+    prix: "500 F",
+    note: 4
+  }
+];
+
+const autresCategories = {
+  Cupcakes: [
+ {
+    nom: "CupCake",
+    img: "../images/cupcake1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+ {
+    nom: "CupCake",
+    img: "../images/cupcake2.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cupcake",
+    img: "../images/cupcake3.jpg",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "CupCake",
+    img: "../images/cupcake5.JPG",
+    prix: "500 F",
+    note: 4
+  },
+   {
+    nom: "CupCake",
+    img: "../images/cupcake6.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  ],
+
+  Cakes: [
+    {
+    nom: "Cake",
+    img: "../images/cake4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Cake",
+    img: "../images/cake3.JPG",
+    prix: "500 F",
+    note: 4
+  },
+     {
+    nom: "cake",
+    img: "../images/cake1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+     {
+    nom: "Cake",
+    img: "../images/IMG_3001.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Cake",
+    img: "../images/IMG_2999.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Cake",
+    img: "../images/cake5.JPG",
+    prix: "700 F",
+    note: 5
+  }
+  ],
+
+  Gateaux_de_marriage: [
+      {
+    nom: "Marriage",
+    img: "../images/etage7.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Marriage",
+    img: "../images/etage8.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Marriage",
+    img: "../images/etage4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Marriage",
+    img: "../images/etage3.JPG",
+    prix: "500 F",
+    note: 4
+  },
+  {
+    nom: "Marriage",
+    img: "../images/etage1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "marriage",
+    img: "../images/etage2.JPG",
+    prix: "500 F",
+    note: 4
+  }
+  ],
+
+  Genoise: [
+  {
+    nom: "Genoise",
+    img: "../images/IMG_2830.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Genoise",
+    img: "../images/IMG_2827.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Genoise",
+    img: "../images/img_4.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Genoise",
+    img: "../images/img_2.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Genoise",
+    img: "../images/img_1.JPG",
+    prix: "500 F",
+    note: 4
+  },
+    {
+    nom: "Genoise",
+    img: "../images/img_3.JPG",
+    prix: "500 F",
+    note: 4
+  }
+  ]
+};
+
+function generateStars(note) {
+  let stars = "";
+  for (let i = 1; i <= 5; i++) {
+    stars += i <= note ? "★" : "☆";
+  }
+  return stars;
+}
+
+function showCategory(cat) {
+  const content = document.getElementById("content");
+  const title = document.getElementById("category-title");
+
+  title.textContent = cat.toUpperCase();
+
+  content.innerHTML = "";
+
+  let items = [];
+  if (cat === "Tous") items = boissons;
+  else items = autresCategories[cat];
+
+  if (items.length === 0) {
+    content.innerHTML = "<p>Pas d'éléments pour cette catégorie.</p>";
+    return;
+  }
+
+  items.forEach(item => {
+    const drink = document.createElement("div");
+    drink.className = "drink";
+    drink.innerHTML = `
+      <img src="${item.img}" alt="${item.nom}">
+      <div class="stars">${generateStars(item.note)}</div>
+      <div class="name">${item.nom}</div>
+      <div class="price">${item.prix}</div>
+      <div class="btn-orange"><a class="icon" href="https://wa.me/+237699740930?text=Bonjour, j'aime bien vos elements "><i class="fa-brands fa-whatsapp"></i> Commandez</div>
+    `;
+    content.appendChild(drink);
+  });
+}
+
+showCategory('Tous');
---
+++ JS/scriptCake.js
@@ -0,0 +1,39 @@
+const navbar=document.getElementById("burge");
+const afficheBurg=document.getElementById("burgeur");
+const masqueBurg=document.getElementById("burgeur");
+
+afficheBurg.addEventListener("click",()=>{
+    // navbar.classList.toggle ("visible");
+    navbar.classList.toggle ("display");
+
+});
+
+// masqueBurg.addEventListener("click",()=>{
+//     navbar.classList.toggle ("masque");
+// });
+
+/*************************************************************************************************************/
+// pour envoyer les infos d'un formulaire via whatsapp
+
+  document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Récupérer les données du formulaire
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+    const telephone = document.getElementById('telephone').value;
+    const message = document.getElementById('message').value;
+
+    // Créer le message à envoyer
+    const text = `Bonjour, je m'appelle ${name} ${surname}. Mon email est ${email} et mon numero est le ${telephone}. Mon message est le suivant: ${message}`;
+    const phoneNumber = "+237699740930"; // Remplace par le numéro WhatsApp (sans le +)
+
+    // Créer l'URL WhatsApp
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`;
+
+    // Rediriger vers WhatsApp
+    window.open(url, '_blank');
+  });
+
+  
